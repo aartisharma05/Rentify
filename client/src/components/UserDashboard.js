c8437f88be6baec710d01982277d3314e308a390
aartisharma05 <aartisharma.official1@gmail.com>
2024-05-27 01:49:30 +0530

    feat:"userDashboard"

--- client/src/components/UserDashboard.js
+++ client/src/components/UserDashboard.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import SellerCard from "./SellerCard";
 const UserDashboard = () => {
-  return <div>UserDash</div>;
+  return (
+    <>
+      <h1 className="border-2 border-dotted border-gray-600 p-2 m-2 rounded-lg text-center font-bold text-4xl">
+        User Dashboard
+      </h1>
+
+      <span className="flex flex-col items-center justify-center">
+        <SellerCard />
+      </span>
+    </>
+  );
 };
 
 export default UserDashboard;
